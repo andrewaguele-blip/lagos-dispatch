7b6f4669f2cedc557aa583987f27cf83a6f7daac
andrewaguele-blip <andrewaguele@gmail.com>
2026-01-30 18:55:30 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -64,7 +64,7 @@ export default function Home() {
   >
     {/* IMAGE */}
     <img
-      src="/rider-services.jpg"
+      src="/rider-services.png"
       alt="Lagos Dispatch Rider"
       style={{
         width: '100%',
